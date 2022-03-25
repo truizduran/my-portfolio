9a8417522a9cb0ee55b6cd52128fad8e53e65666
truizduran <truizd92@gmail.com>
2022-03-25 18:09:44 +0100

    add function openNav

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+// Open and close sidebar
+
+
+function openNav(){
+    let anchoVentana = window.innerWidth;
+
+    if(anchoVentana > 770){
+        document.getElementById("mySidebar").style.width = "60%";
+        document.getElementById("mySidebar").style.display = "block";
+        
+    }else{
+        
+        document.getElementById("mySidebar").style.width = "100%";
+        document.getElementById("mySidebar").style.display = "block";
+        
+    }
+
+    
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+
+//window.innerWidth
+//screen.width
